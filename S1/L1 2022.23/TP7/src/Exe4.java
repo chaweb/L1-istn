91b5e227eb948b39304d90e798414fada0eef124
chaweb <46398015+chaweb@users.noreply.github.com>
2023-08-05 23:09:22 +0200

    prout

--- S1/L1 2022.23/TP7/src/Exe4.java
+++ S1/L1 2022.23/TP7/src/Exe4.java
@@ -3,13 +3,15 @@ package tp7;
 public class Exe4 {
 
     public static int[][] copieurTab2D(int[][] r2) {
+
         int[][] tabR = new int[r2.length][];
+
         for (int i = 0; i < tabR.length; i++) {
             tabR[i] = new int[r2[i].length];
 
-            for (int j = 0; j < tabR[i].length; j++) {
+            for (int j = 0; j < tabR[i].length; j++)
                 tabR[i][j] = r2[i][j];
-            }
+
         }
         return tabR;
     }
@@ -17,24 +19,21 @@ public class Exe4 {
     public static void afficheTab2D(int[][] r2) {
 
         System.out.print("+");
-        for (int i = 0; i < r2[0].length; i++) {
+
+        for (int i = 0; i < r2[0].length; i++)
             System.out.print("--+");
-        }
+
 
         for (int i = 0; i < r2.length; i++) {
             System.out.print("\n");
-            for (int j = 0; j < r2[i].length; j++) {
+
+            for (int j = 0; j < r2[i].length; j++)
                 System.out.print("|" + r2[i][j] + ((r2[i][j] - r2[i][j] % 10) >= 10 ? "" : " "));
-            }
 
-            System.out.print("|");
+            System.out.print("|\n+");
 
-            System.out.print("\n");
-
-            System.out.print("+");
-            for (int j = 0; j < r2[0].length; j++) {
+            for (int j = 0; j < r2[0].length; j++)
                 System.out.print("--+");
-            }
         }
 
         System.out.print("\n\n");
@@ -43,11 +42,10 @@ public class Exe4 {
     // Écrire une fonction qui renvoie la somme, case par case, de deux rectangles
     public static int[][] sommeRectangles(int[][] tab1, int[][] tab2) {
         int[][] tabR = copieurTab2D(tab1);
-        for (int i = 0; i < tabR.length; i++) {
-            for (int y = 0; y < tabR[i].length; y++) {
+        for (int i = 0; i < tabR.length; i++)
+            for (int y = 0; y < tabR[i].length; y++)
                 tabR[i][y] += tab2[i][y];
-            }
-        }
+
         return tabR;
     }
 
@@ -55,9 +53,9 @@ public class Exe4 {
     public static int[][] symetrieHorizontale(int[][] tab1) {
         int[][] tabR = copieurTab2D(tab1);
         for (int i = 0; i < tabR.length; i++) {
-            for (int y = 0; y < tabR[i].length; y++) {
+            for (int y = 0; y < tabR[i].length; y++)
                 tabR[i][y] = tab1[tabR.length - i - 1][y];
-            }
+
         }
         return tabR;
     }
@@ -66,25 +64,41 @@ public class Exe4 {
     public static int[][] symetrieVerticale(int[][] tab1) {
         int[][] tabR = copieurTab2D(tab1);
         for (int i = 0; i < tabR.length; i++) {
-            for (int y = 0; y < tabR[i].length; y++) {
+            for (int y = 0; y < tabR[i].length; y++)
                 tabR[i][y] = tab1[i][tabR[i].length - y - 1];
-            }
+
         }
         return tabR;
     }
     // Écrire une fonction qui renvoie la transposition du rectangle d'entrée
 
+	public static int[][] transposition(int[][] tab1) {
+		int[][] tabR = new int[tab1[0].length][tab1.length];
+
+		for (int i = 0; i < tab1.length; i++)
+			for (int j = 0; j < tab1[i].length; j++)
+				tabR[j][i] = tab1[i][j];
+
+		return tabR;
+	}
+
     // Écrire une fonction qui renvoie la rotation du rectangle d'entrée
-
+	public static int[][] rotation(int[][] tab1) {
+		return symetrieHorizontale(transposition(tab1));
+	}
     public static void main(String[] args) {
-        int[][] rectangle = {{1, 12, 20, 8, 17, 6, 13},
-                {14, 2, 10, 21, 9, 16, 5},
-                {18, 19, 3, 4, 7, 11, 15}};
+        int[][] rectangle = {
+				{1, 12, 20, 8, 17, 6, 13},
+				{14, 2, 10, 21, 9, 16, 5},
+				{18, 19, 3, 4, 7, 11, 15}
+		};
 
 
-        int[][] rectangle2 = {{3, 5, 54, 1, 7, 10, 3},
-                {2, 2, 12, 21, 13, 21, 0},
-                {18, 19, 3, 4, 7, 11, 5}};
+        int[][] rectangle2 = {
+				{3, 5, 54, 1, 7, 10, 3},
+				{2, 2, 12, 21, 13, 21, 0},
+				{18, 19, 3, 4, 7, 11, 5}
+		};
 
         afficheTab2D(sommeRectangles(rectangle, rectangle2));
 
@@ -92,5 +106,8 @@ public class Exe4 {
 
         afficheTab2D(symetrieHorizontale(rectangle));
         afficheTab2D(symetrieVerticale(rectangle));
+
+		afficheTab2D(transposition(rectangle));
+		afficheTab2D(rotation(rectangle));
     }
 }
